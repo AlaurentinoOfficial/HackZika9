3470f95c6df8d3dcbc4db4dadc49fc2353e0239e
Anderson Laurentino <alaurentino.br@gmail.com>
2017-08-12 14:37:07 -0300

    Change baby page

--- app/src/main/java/org/passinhos/passinhos/IncomeFragment.java
+++ app/src/main/java/org/passinhos/passinhos/IncomeFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 import org.passinhos.passinhos.Adapters.IncomeAdapter;
 import org.passinhos.passinhos.Models.Income;
--- app/src/main/java/org/passinhos/passinhos/NavigationActivity.java
+++ app/src/main/java/org/passinhos/passinhos/NavigationActivity.java
@@ -6,9 +6,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,15 +23,20 @@ public class NavigationActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_baby:
                     fragment = new BabyFragment();
+                    toolbar.setTitle(R.string.title_baby);
                     break;
                 case R.id.navigation_map:
                     fragment = new MapsFragment();
+                    toolbar.setTitle(R.string.title_map);
+                    //toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
                     break;
                 case R.id.navigation_income:
                     fragment = new IncomeFragment();
+                    toolbar.setTitle(R.string.title_income);
                     break;
                 case R.id.navigation_account:
                     fragment = new AccountFragment();
+                    toolbar.setTitle(R.string.title_account);
                     break;
             }
 
@@ -41,6 +49,10 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        setSupportActionBar(toolbar);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
